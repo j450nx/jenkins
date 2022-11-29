da5fd4a91125feaab9317ddf12c422e02e1b2a42
Jason <j450nx@gmail.com>
2022-11-29 11:18:36 -0500

    githook test

--- Hello.java
+++ Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Jenkins Poll SCM test");
+		System.out.println("githook test");
+		
 	}
 
 }
